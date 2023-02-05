f18fc7f941566d65db608ad5b6aa6b4cf3a8ea5c
Toni Margan <toni.margan@gmail.com>
2023-02-05 18:57:41 +0100

    Added zoom

--- js/controls.js
+++ js/controls.js
@@ -5,7 +5,6 @@ export default function createControls(camera) {
     const controls = new OrbitControls(camera, canvas);
     controls.enableDamping = true;
     controls.enablePan = false;
-    controls.enableZoom = false;
     controls.autoRotate = true;
     controls.autoRotateSpeed = 5;
     return controls;
